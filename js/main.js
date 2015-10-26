38953e793258ff97daefe4bbc1496cf5b7751e00
Nirawit <sixfingeredamish@gmail.com>
2015-10-26 09:39:21 -0700

    changes made in class

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@
 
 /* Use clever selections and event handlers to assign the described interactivity */
 
+
+$("#change-content").click(function() {
+    $("#selector").attr("#newContent");
+
+});
+
+
+$("#add-at-end").click(function() {
+    $("#selector").append("#newContent");
+
+});
+
